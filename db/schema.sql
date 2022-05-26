9b7e5d7373e73f5e56b6e08122b0bbf36d5ed88f
Katy Brown <katybr35@gmail.com>
2022-05-26 15:40:42 -0500

    installed console.table

--- db/schema.sql
+++ db/schema.sql
@@ -11,8 +11,9 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary INT NOT NULL,
+    salary DECIMAL,
     department_id INT NOT NULL
+    
  );
 
  CREATE TABLE employee (
@@ -20,5 +21,8 @@ CREATE TABLE role (
      first_name VARCHAR(30),
      last_name VARCHAR(30),
      role_id INT NOT NULL,
-     manager_id INT NOT NULL
+     manager_id INT,
+     FOREIGN KEY (manager_id)
+     REFERENCES department(id)
+    ON DELETE SET NULL
  );
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const mysql = require('mysql2');
+const express = require("express");
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const consoleTable = require ('console.table');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -7,9 +9,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "Ethanhenry01!",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
 
-app.listen(PORT, () =>{
-    console.log(`Server is running 🏃 on ${PORT}`)
+app.use((req, res) => {
+  res.status(404).end();
 });
 
-
+app.listen(PORT, () => {
+  console.log(`Server is running 🏃 on ${PORT}`);
+});
